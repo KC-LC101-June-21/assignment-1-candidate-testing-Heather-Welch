95aacbfd6603be971a86ab801ded3569daf239c6
Heather Welch (she/her) <84741947+Heather-Welch@users.noreply.github.com>
2021-06-10 18:23:44 +0000

    Define questions and correctAnswers variables as arrays. Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses. Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,16 +1,24 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
+
+for (i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  console.log(`You answered: ${candidateAnswers[i]}`);
+  console.log(`The correct answer is: ${correctAnswers[i]}`);
+}
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let candidateAnswer = "";
+
+
 
 
 function askForName() {
@@ -28,10 +36,8 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 if (candidateAnswer === correctAnswer)  {
-  console.log("Your answer is correct!");
-  } else  {
-    console.log("Your answer is incorrect.");
-  }
+  
+}
 
   let grade;
   
