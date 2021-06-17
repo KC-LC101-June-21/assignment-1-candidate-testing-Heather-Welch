a7d603b47f877ad87ccad4f41196739f2f72a6eb
Heather Welch (she/her) <84741947+Heather-Welch@users.noreply.github.com>
2021-06-17 19:15:05 +0000

    Updated CandidateName

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+let question = "Enter your name: ";
+let correctAnswer = "";
 let candidateAnswer = "";
 let questions = ["1) Who was the first American woman in space? ","2) True or false: 5 kilometer == 5000 meters? ","3) (5 + 3)/2 * 10 = ? ","4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","5) What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
@@ -64,7 +64,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log(space);
-  console.log("Hello, " + candidateName + "!");
+  console.log("Candidate name: " + candidateName);
   console.log(space);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
